64e117b09d610ac2810a323f76bf6ef27ed93f9b
Q <q@q-continuum.net>
2014-01-08 02:04:10 -0700

    Beginnings of the simple record_to_ogg package which should... record things to ogg. Right now it just has an encode_to_ogg function which calls out to oggenc2, included. no seutp script yet.

---
+++ record_to_ogg.py
@@ -0,0 +1,9 @@
+import subprocess
+import sound_lib
+import resource_finder
+
+def encode_to_ogg(filename, quality=4.5):
+ subprocess.check_call(r'"%s" -q %r "%s"' % (resource_finder.find_application_resource('oggenc2.exe'), quality, filename))
+ created_filename = '%s.ogg' % os.path.splitext(filename)[0]
+ return created_filename
+
