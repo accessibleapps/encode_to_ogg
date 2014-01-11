48338fc9861837a7091263c40268f50cb840c694
Christopher Toth <q.alpha@gmail.com>
2014-01-10 23:30:01 -0700

    Wrote a find_datafiles function

--- encode_to_ogg.py
+++ encode_to_ogg.py
@@ -4,6 +4,11 @@ import subprocess
 import sound_lib
 import resource_finder
 
+def find_datafiles():
+ import sys
+ path = os.path.split(os.path.abspath(sys.modules[find_datafiles.__module__].__file__))[0]
+ return [(), [os.path.join(path, 'oggenc2.exe')]]
+
 def encode_to_ogg(filename, quality=4.5):
  subprocess.check_call(r'"%s" -q %r "%s"' % (resource_finder.find_application_resource('oggenc2.exe'), quality, filename))
  created_filename = '%s.ogg' % os.path.splitext(filename)[0]
