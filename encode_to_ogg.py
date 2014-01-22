613133857392a97a4e4b2631b18162af8e2b067e
Christopher Toth <q.alpha@gmail.com>
2014-01-22 12:44:09 -0700

    Fixed find_datafiles

--- encode_to_ogg.py
+++ encode_to_ogg.py
@@ -7,7 +7,7 @@ import resource_finder
 def find_datafiles():
  import sys
  path = os.path.split(os.path.abspath(sys.modules[find_datafiles.__module__].__file__))[0]
- return ['', [os.path.join(path, 'oggenc2.exe')]]
+ return [('', [os.path.join(path, 'oggenc2.exe')])]
 
 def encode_to_ogg(filename, quality=4.5):
  subprocess.check_call(r'"%s" -q %r "%s"' % (resource_finder.find_application_resource('oggenc2.exe'), quality, filename))
